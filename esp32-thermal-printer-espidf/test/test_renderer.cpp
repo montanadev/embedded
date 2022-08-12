e8dab4cbaced4e0b358973ce483b254dc8130238
Nathan Evans <nathan@de1ux.com>
2022-08-11 23:14:31 -0600

    Clean up tests

--- esp32-thermal-printer-espidf/test/test_renderer.cpp
+++ esp32-thermal-printer-espidf/test/test_renderer.cpp
@@ -8,90 +8,75 @@
  *               and support checkboxes
  */
 
-void test_split(void) {
+void assert_rendered(string input, vector<vector<string>> expected)
+{
+  auto actual = renderer(input);
+
+  TEST_ASSERT_EQUAL(expected.size(), actual.size());
+  for (int x = 0; x < actual.size(); x++)
+  {
+    for (int y = 0; y < actual[x].size(); y++)
+    {
+      TEST_ASSERT_EQUAL_STRING(expected[x][y].c_str(), actual[x][y].c_str());
+    }
+  }
+}
+
+void test_split(void)
+{
   string a = "|jkl|mno|";
   auto b = split(a, '\|');
-  
+
   TEST_ASSERT_EQUAL(3, b.size());
   TEST_ASSERT_EQUAL_STRING("", b[0].c_str());
   TEST_ASSERT_EQUAL_STRING("jkl", b[1].c_str());
   TEST_ASSERT_EQUAL_STRING("mno", b[2].c_str());
 }
 
-void test_newlines(void) {
-  string a = "a\nb\nc\nd";
-  auto result = renderer(a);
-
-  vector<vector<string>> expected{
-      {"println", "a"},
-      {"println", "b"},
-      {"println", "c"},
-      {"println", "d"},
-  };
-
-  TEST_ASSERT_EQUAL(expected.size(), result.size());
-  for (int x = 0; x < result.size(); x++)
-  {
-    for (int y = 0; y < result[x].size(); y++)
-    {
-      TEST_ASSERT_EQUAL_STRING(expected[x][y].c_str(), result[x][y].c_str());
-    }
-  }
+void test_newlines(void)
+{
+  assert_rendered(
+      "a\nb\nc\nd",
+      {
+          {"println", "a"},
+          {"println", "b"},
+          {"println", "c"},
+          {"println", "d"},
+      });
 }
 
 void test_renderer_table_formatting(void)
 {
   // TODO - support single row table
   // TODO - what if the table is wider than the receipt paper?
-  string a =
+
+  assert_rendered(
       "|name|fun hobby|\n"
       "|-|-|\n"
-      "|nathan|embedded development|\n";
-  vector<vector<string>> result = renderer(a);
+      "|nathan|embedded development|\n",
+      {
+          {"boldOn"},
+          {"println", "| name   | fun hobby            |"},
+          {"println", "| ------ | -------------------- |"},
+          {"boldOff"},
+          {"println", "| nathan | embedded development |"},
+      });
 
-  vector<vector<string>> expected{
-      {"boldOn"},
-      {"println", "| name   | fun hobby            |"},
-      {"println", "| ------ | -------------------- |"},
-      {"boldOff"},
-      {"println", "| nathan | embedded development |"},
-  };
-
-  TEST_ASSERT_EQUAL(expected.size(), result.size());
-  for (int x = 0; x < result.size(); x++)
-  {
-    for (int y = 0; y < result[x].size(); y++)
-    {
-      TEST_ASSERT_EQUAL_STRING(expected[x][y].c_str(), result[x][y].c_str());
-    }
-  }
-
-  a =
+  assert_rendered(
       "| Option | Description |\n"
       "| ------ | ----------- |\n"
       "| data   | path to data |\n"
       "| engine | engine to be used |\n"
-      "| ext    | extension to save |\n";
-  result = renderer(a);
-
-  expected = {
-      {"boldOn"},
-      {"println", "| Option | Description       |"},
-      {"println", "| ------ | ----------------- |"},
-      {"boldOff"},
-      {"println", "| data   | path to data      |"},
-      {"println", "| engine | engine to be used |"},
-      {"println", "| ext    | extension to save |"},
-  };
-
-  TEST_ASSERT_EQUAL(expected.size(), result.size());
-  for (int x = 0; x < result.size(); x++)
-  {
-    for (int y = 0; y < result[x].size(); y++)
-    {
-      TEST_ASSERT_EQUAL_STRING(expected[x][y].c_str(), result[x][y].c_str());
-    }
-  }
+      "| ext    | extension to save |\n",
+      {
+          {"boldOn"},
+          {"println", "| Option | Description       |"},
+          {"println", "| ------ | ----------------- |"},
+          {"boldOff"},
+          {"println", "| data   | path to data      |"},
+          {"println", "| engine | engine to be used |"},
+          {"println", "| ext    | extension to save |"},
+      });
 }
 
 void test_renderer_split_to_lines(void)
@@ -109,117 +94,72 @@ void test_renderer_split_to_lines(void)
 
 void test_renderer_simple_text_formatting(void)
 {
-  string a =
-      "** bold **\n#hi";
-  vector<vector<string>> result = renderer(a);
-  
-  //std::cout << debug(result) << endl;
-
-  vector<vector<string>> expected{
-      {"boldOn"},
-      {"println", " bold "},
-      {"boldOff"},
-      {"setSize", "L"},
-      {"println", "hi"},
-      {"setSize", "S"},
-  };
-
-  TEST_ASSERT_EQUAL(expected.size(), result.size());
-  for (int x = 0; x < result.size(); x++)
-  {
-    for (int y = 0; y < result[x].size(); y++)
-    {
-      TEST_ASSERT_EQUAL_STRING(expected[x][y].c_str(), result[x][y].c_str()); 
-    }
-  }
+  assert_rendered(
+      "** bold **\n#hi",
+      {
+          {"boldOn"},
+          {"println", " bold "},
+          {"boldOff"},
+          {"setSize", "L"},
+          {"println", "hi"},
+          {"setSize", "S"},
+      });
 }
 
-void test_renderer_horizontal_rule(void) {
-  string a = 
+void test_renderer_horizontal_rule(void)
+{
+  assert_rendered(
       "--\n"
-      "---\n"
-      " ----   \n";
-  vector<vector<string>> result = renderer(a);
-
-  vector<vector<string>> expected{
-      {"println", "--"},
-      {"setSize", "S"},
-      {"boldOn"},
-      {"println", "--------------------------------"},
-      {"boldOff"},
-      {"setSize", "S"},
-      {"boldOn"},
-      {"println", "--------------------------------"},
-      {"boldOff"},
-  };
-
-  TEST_ASSERT_EQUAL(expected.size(), result.size());
-  for (int x = 0; x < result.size(); x++)
-  {
-    for (int y = 0; y < result[x].size(); y++)
-    {
-      TEST_ASSERT_EQUAL_STRING(expected[x][y].c_str(), result[x][y].c_str());
-    }
-  }
+      "--- \n"
+      " ----  \n",
+      {
+          {"println", "--"},
+          {"setSize", "S"},
+          {"boldOn"},
+          {"println", "--------------------------------"},
+          {"boldOff"},
+          {"setSize", "S"},
+          {"boldOn"},
+          {"println", "--------------------------------"},
+          {"boldOff"},
+      });
 }
 
 void test_renderer_bold_header_formatting(void)
 {
-  string a =
-      "# **bold**\n";
-  vector<vector<string>> result = renderer(a);
-
-  vector<vector<string>> expected{
-      {"setSize", "L"},
-      {"boldOn"},
-      {"println", "bold"},
-      {"boldOff"},
-      {"setSize", "S"},
-  };
-
-  TEST_ASSERT_EQUAL(expected.size(), result.size());
-  for (int x = 0; x < result.size(); x++)
-  {
-    for (int y = 0; y < result[x].size(); y++)
-    {
-      TEST_ASSERT_EQUAL_STRING(expected[x][y].c_str(), result[x][y].c_str());
-    }
-  }
+  assert_rendered(
+      "# **bold**\n",
+      {
+          {"setSize", "L"},
+          {"boldOn"},
+          {"println", "bold"},
+          {"boldOff"},
+          {"setSize", "S"},
+      });
 }
 
 void test_renderer_simple_heading(void)
 {
-  string a =
+  assert_rendered(
       " # h1\n"
       "  ## h2\n"
       "### h3\n"
       "  #### h4\n"
-      "   ##### h5\n";
-  vector<vector<string>> result = renderer(a);
-
-  vector<vector<string>> expected{
-      {"setSize", "L"},
-      {"println", "h1"},
-      {"setSize", "S"},
-      {"setSize", "M"},
-      {"println", "h2"},
-      {"setSize", "S"},
-      {"setSize", "S"},
-      {"println", "h3"},
-      {"setSize", "S"},
-      {"println", "h4"},
-      {"setSize", "S"},
-      {"println", "h5"},
-  };
-
-  TEST_ASSERT_EQUAL(expected.size(), result.size());
-  for (int x = 0; x < result.size(); x++)
-  {
-    for (int y = 0; y < result[x].size(); y++)
-    {
-      TEST_ASSERT_EQUAL_STRING(expected[x][y].c_str(), result[x][y].c_str());
-    }
-  }
+      "   ##### h5\n",
+      {
+          {"setSize", "L"},
+          {"println", "h1"},
+          {"setSize", "S"},
+          {"setSize", "M"},
+          {"println", "h2"},
+          {"setSize", "S"},
+          {"setSize", "S"},
+          {"println", "h3"},
+          {"setSize", "S"},
+          {"println", "h4"},
+          {"setSize", "S"},
+          {"println", "h5"},
+      });
 }
 
 int main()
