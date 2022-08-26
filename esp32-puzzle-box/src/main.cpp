2da2d862a39bcf228793696249fb7ef6be72fd9e
Nathan Evans <nathan@de1ux.com>
2022-08-25 18:59:07 -0600

    Refactor games into indiv files, make reusable utils

--- esp32-puzzle-box/src/main.cpp
+++ esp32-puzzle-box/src/main.cpp
@@ -4,13 +4,15 @@
 #include <Adafruit_MPU6050.h>
 #include <esp_log.h>
 #include <vector>
+#include "../lib/game_1.cpp"
+#include "../lib/game_2.cpp"
+#include "../lib/game_3.cpp"
 
 extern "C"
 {
 #define NEOPIXEL_LED_PIN 23
 #define NEOPIXEL_LED_COUNT 20
-#define BUTTON_PIN 4
-#define BUTTON_LED 2
+    
 
     // Adafruit_MPU6050 xyz gyro
     Adafruit_MPU6050 mpu;
@@ -23,281 +25,22 @@ extern "C"
     uint32_t green = led_strip.Color(0, 255, 0);
     uint32_t blue = led_strip.Color(0, 0, 255);
 
-    bool readButton()
-    {
-        int buttonState = digitalRead(BUTTON_PIN);
-        return buttonState == HIGH;
-    }
-
-    void failedAnimation()
-    {
-        for (int times = 0; times < 1; times++)
-        {
-            for (int i = 0; i < led_strip.numPixels(); i++)
-            {
-                led_strip.setPixelColor(i, white);
-                delay(1);
-            }
-            led_strip.show();
-            delay(100);
-            for (int i = 0; i < led_strip.numPixels(); i++)
-            {
-                led_strip.setPixelColor(i, red);
-                delay(1);
-            }
-            led_strip.show();
-            delay(100);
-        }
-    }
-
-    void victoryAnimation()
-    {
-        for (int times = 0; times < 5; times++)
-        {
-            for (int i = 0; i < led_strip.numPixels(); i++)
-            {
-                led_strip.setPixelColor(i, white);
-                delay(1);
-            }
-            led_strip.show();
-            delay(100);
-            for (int i = 0; i < led_strip.numPixels(); i++)
-            {
-                led_strip.setPixelColor(i, blue);
-                delay(1);
-            }
-            led_strip.show();
-            delay(100);
-        }
-    }
-
     void game1()
     {
-        for (int games = 0; games < 3; games++)
-        {
-            // initialize game
-            for (int i = 0; i < led_strip.numPixels(); i++)
-            {
-                led_strip.setPixelColor(i, white);
-            }
-            led_strip.show();
-
-            int goalLed = random(0, 20);
-
-            bool playing = true;
-            while (playing)
-            {
-                led_strip.setPixelColor(goalLed, green);
-                for (int i = 0; i < led_strip.numPixels(); i++)
-                {
-                    int prev = i - 1;
-                    if (i == 0)
-                    {
-                        prev = led_strip.numPixels() - 1;
-                    }
-                    // erase behind the current pixel, if its not the goal
-                    if (prev != goalLed)
-                    {
-                        led_strip.setPixelColor(prev, white);
-                    }
-
-                    if (i != goalLed)
-                    {
-                        led_strip.setPixelColor(i, blue);
-                    }
-
-                    led_strip.show();
-                    if (readButton())
-                    {
-                        ESP_LOGI("game1", "i=%d goalLed=%d", i, goalLed);
-                        if (i == goalLed)
-                        {
-                            playing = false;
-                            victoryAnimation();
-                            break;
-                        }
-                        else
-                        {
-                            failedAnimation();
-                            for (int i = 0; i < led_strip.numPixels(); i++)
-                            {
-                                led_strip.setPixelColor(i, white);
-                            }
-                            led_strip.setPixelColor(goalLed, green);
-                            led_strip.show();
-                        }
-                    }
-                    // TODO - move the delay into the button and offer more checks
-                    if (games == 0)
-                    {
-                        delay(100);
-                    }
-                    if (games == 1)
-                    {
-                        delay(80);
-                    }
-                    if (games == 2)
-                    {
-                        delay(50);
-                    }
-                }
-            }
-        }
+        Game1 g = Game1(led_strip);
+        g.run();
     }
 
     void game2()
     {
-        for (int games = 0; games < 3; games++)
-        {
-            int goalDepth = random(0, 500);
-            int padding = 20;
-            while (1)
-            {
-                VL53L0X_RangingMeasurementData_t measure;
-                lox.rangingTest(&measure, false);
-
-                if ((goalDepth - padding) < measure.RangeMilliMeter && measure.RangeMilliMeter < (goalDepth + padding))
-                {
-                    // within area, win!
-                    break;
-                }
-
-                // reset pixels to white
-                for (int i = 0; i < led_strip.numPixels(); i++)
-                {
-                    led_strip.setPixelColor(i, white);
-                }
-                int pixelGoalPost = floor(goalDepth / 25);
-                int pixelCurrentDepth = floor(measure.RangeMilliMeter / 25);
-                led_strip.setPixelColor(pixelGoalPost, green);
-                led_strip.setPixelColor(pixelCurrentDepth, blue);
-                led_strip.show();
-                delay(100);
-            }
-            victoryAnimation();
-        }
-    }
-
-    void clearDisplay()
-    {
-        for (int i = 0; i < led_strip.numPixels(); i++)
-        {
-            led_strip.setPixelColor(i, white);
-        }
-        led_strip.show();
-    }
-
-    // determineMovement takes a raw movement from the gyro, normalizes it against the
-    // baseline for that axis (the mpu reports different baseline values for each axis)
-    // and then cuts off at a given threshold, to remove noise
-    // if movement is below threshold, 0 is returned
-    int determineMovement(int movement, int baseline, int threshold)
-    {
-        if (movement < (baseline - threshold))
-        {
-            return abs(movement / 10);
-        }
-        if (movement > (baseline + threshold))
-        {
-            return -1 * abs(movement / 10);
-        }
-        return 0;
-    }
-
-    void displayRanges(int pos, std::vector<std::vector<int>> ranges)
-    {
-        for (auto range : ranges)
-        {
-            int start = range[0];
-            int stop = range[1];
-            int pin = range[2];
-            if (pos < stop && pos > start)
-            {
-                led_strip.setPixelColor(pin, green);
-                break;
-            }
-        }
-        if (pos < ranges[0][0]) {
-            led_strip.setPixelColor(ranges[0][2], green);
-        }
-        if (pos > ranges[ranges.size() - 1][1]) {
-            led_strip.setPixelColor(ranges[ranges.size() - 1][2], green);
-        }
-    }
-
-    int clamp(int i, int low, int high) {
-        if (i < low) {
-            return low;
-        }
-        if (i > high) {
-            return high;
-        }
-        return i;
+        Game2 g = Game2(led_strip, lox);
+        g.run();
     }
 
     void game3()
     {
-        int threshold = 400;
-        int xPos = 0;
-        int yPos = 0;
-        int zPos = 0;
-
-        clearDisplay();
-        led_strip.setPixelColor(2, green);
-        led_strip.show();
-
-        while (1)
-        {
-            sensors_event_t a, g, temp;
-            mpu.getEvent(&a, &g, &temp);
-
-            int x = floor(g.gyro.x * 100); // x normalizes at -14
-            int y = floor(g.gyro.y * 100); // y normalizes at 1
-            int z = floor(g.gyro.z * 100); // z at 3
-
-            // ESP_LOGI("game3", "Rotation X: %d, Y: %d, Z: %d", x, y, z);
-
-            // TODO - determine what movement looks like
-            xPos += clamp(determineMovement(x, -14, threshold), -250, 250);
-            yPos += clamp(determineMovement(y, 1, threshold), -250, 250);
-            zPos += clamp(determineMovement(z, 3, threshold), -250, 250);
-
-            //ESP_LOGI("game3", "xpos=%d", xPos);
-            //ESP_LOGI("game3", "ypos=%d", yPos);
-            ESP_LOGI("game3", "zpos=%d z=%d", zPos, z);
-
-            clearDisplay();
-            displayRanges(
-                xPos,
-                {
-                    {-250, -150, 0},
-                    {-150, -50, 1},
-                    {-50, 50, 2},
-                    {50, 150, 3},
-                    {150, 250, 4},
-                });
-            displayRanges(
-                yPos,
-                {
-                    {-250, -150, 5},
-                    {-150, -50, 6},
-                    {-50, 50, 7},
-                    {50, 150, 8},
-                    {150, 250, 9},
-                });
-            displayRanges(
-                zPos,
-                {
-                    {-250, -150, 10},
-                    {-150, -50, 11},
-                    {-50, 50, 12},
-                    {50, 150, 13},
-                    {150, 250, 14},
-                });
-            led_strip.show();
-            
-            delay(100);
-        }
+        Game3 g = Game3(led_strip, mpu);
+        g.run();
     }
 
     void app_main()
@@ -338,11 +81,14 @@ extern "C"
         pinMode(BUTTON_LED, OUTPUT);
         pinMode(BUTTON_PIN, INPUT);
 
-        game3();
-        victoryAnimation();
-        game1();
-        victoryAnimation();
-        game2();
-        victoryAnimation();
+        // initialize games
+        Game1 g1 = Game1(led_strip);
+        Game2 g2 = Game2(led_strip, lox);
+        Game3 g3 = Game3(led_strip, mpu);
+
+        // play!
+        g1.run();
+        g2.run();
+        g3.run();
     }
 }
