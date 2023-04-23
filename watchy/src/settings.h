f9890d0ab2d3a37c8a4e1a83a65bcfe6c495a3f4
Nathan Evans <nathan@de1ux.com>
2023-04-22 20:41:51 -0600

    Working redub

--- watchy/src/settings.h
+++ watchy/src/settings.h
@@ -2,27 +2,27 @@
 #define SETTINGS_H
 
 //Weather Settings
-#define CITY_ID "5128581" //New York City https://openweathermap.org/current#cityid
-#define OPENWEATHERMAP_APIKEY "f058fe1cad2afe8e2ddc5d063a64cecb" //use your own API key :)
+#define CITY_ID "2755420" //Gouda https://openweathermap.org/current#cityid
+#define OPENWEATHERMAP_APIKEY "[get your key at openweathermap]" //use your own API key :)
 #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" //open weather api
 #define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
 #define TEMP_LANG "en"
 #define WEATHER_UPDATE_INTERVAL 30 //must be greater than 5, measured in minutes
 //NTP Settings
 #define NTP_SERVER "pool.ntp.org"
-#define GMT_OFFSET_SEC 3600 * -5 //New York is UTC -5 EST, -4 EDT, will be overwritten by weather data
+#define GMT_OFFSET_SEC 3600 * +6 //New York is UTC -5
+#define DST_OFFSET_SEC 3600
 
 watchySettings settings{
-    .cityID = CITY_ID,
-    .weatherAPIKey = OPENWEATHERMAP_APIKEY,
-    .weatherURL = OPENWEATHERMAP_URL,
-    .weatherUnit = TEMP_UNIT,
-    .weatherLang = TEMP_LANG,
-    .weatherUpdateInterval = WEATHER_UPDATE_INTERVAL,
-    .ntpServer = NTP_SERVER,
-    .gmtOffset = GMT_OFFSET_SEC,
-    .dstOffset = 0,
-    .vibrateOClock = true,
+    CITY_ID,
+    OPENWEATHERMAP_APIKEY,
+    OPENWEATHERMAP_URL,
+    TEMP_UNIT,
+    TEMP_LANG,
+    WEATHER_UPDATE_INTERVAL,
+    NTP_SERVER,
+    GMT_OFFSET_SEC,
+    DST_OFFSET_SEC
 };
 
 #endif
